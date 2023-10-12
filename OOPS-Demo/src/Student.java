abf40215e5b612aacb3f2a43f03a8ea84169f7ad
perlajavadevops <72053791+perlajavadevops@users.noreply.github.com>
2023-10-12 20:36:53 +0530

    Initial Projects code

---
+++ OOPS-Demo/src/Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+
+	// properties or data
+	int sno;
+	String firstName;
+	// behivours:
+	public static void getPercentage() {
+		System.out.println("90%");
+	}
+	
+	public static void main(String[] args) {
+		Student.getPercentage();
+	}
+}
